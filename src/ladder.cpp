6db6ef38d37a5ef93ff21511fd88b93c9d2c407b
Melody Fang <melody9fang7@gmail.com>
2025-03-14 03:36:50 +0000

    trying to fix

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -25,7 +25,8 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
 
     for (int i = 0; i < len2; i++) {
         if (str1 == str2.substr(0, i) + str2.substr(i + 1)) return true;
-    }
+        if (i + 1 < len1 && str2 == str2.substr(0, i) + str1.substr(i + 1)) return true;
+}
 
     return false;
 }
@@ -39,9 +40,9 @@ bool is_adjacent(const string& word1, const string& word2){
 }
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     if (word_list.find(end_word) == word_list.end()) return {};
+    if(begin_word == end_word) return {};
     set<string> visited;
     queue<vector<string>> ladder_queue;
-    set<string> dict(word_list.begin(), word_list.end());
     
     ladder_queue.push({begin_word});
     visited.insert(begin_word);
@@ -51,25 +52,20 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         ladder_queue.pop();
         string& last_word = ladder.back();
 
-        for (int j = 0; j < int(last_word.size()); j++) {
-            string temp_word = last_word;
-            for (char c = 'a'; c <= 'z'; c++) {
-                temp_word[j] = c;
-                if (temp_word == last_word) continue;
-
-                if (temp_word == end_word) {
-                    ladder.push_back(temp_word);
-                    return ladder;
-                }
-
-                if (dict.find(temp_word) != dict.end() && visited.find(temp_word) == visited.end()) {
-                    visited.insert(temp_word);
-                    vector<string> new_ladder = ladder;
-                    new_ladder.push_back(temp_word);
-                    ladder_queue.push(new_ladder);
-                }
+        for(auto word: word_list){
+            if(!is_adjacent(word, last_word) || word == last_word){
+                continue;
+            }else if (word == end_word){
+                ladder.push_back(word);
+                return ladder;
             }
-        
+
+            if (word_list.find(word) != word_list.end() && visited.find(word) == visited.end()) {
+                visited.insert(word);
+                vector<string> new_ladder = ladder;
+                new_ladder.push_back(word);
+                ladder_queue.push(new_ladder);
+            }
         }
     }
 
